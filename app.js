facc11237a7a927303b580b1fa8471a5fa4d6d17
Shreeya <shreeya27naik@gmail.com>
2020-08-17 14:20:46 +0530

    first commit

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get("/", function (req,res) {
-  res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
 
-app.post("/", function (req,res) {
+app.post("/", function (req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
 
   var data = {
-    members:[
+    members: [
       {
         email_address: email,
         status: "subscribed",
@@ -28,38 +28,39 @@ app.post("/", function (req,res) {
           LNAME: lastName
         }
       }
-    ]};
+    ]
+  };
 
-const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
 
-const url = "https://us4.api.mailchimp.com/3.0/lists/a206cfced7"
+  const url = "https://us4.api.mailchimp.com/3.0/lists/a206cfced7"
 
-const options = {
-  method: "POST",
-  auth: "shreeya:dac10c5bdc3dd802bca9577a4e84b097-us4"
-}
+  const options = {
+    method: "POST",
+    auth: "shreeya:dac10c5bdc3dd802bca9577a4e84b097-us4"
+  }
 
-const request = https.request(url, options, function (response) {
-if(response.statusCode === 200){
-  res.sendFile(__dirname+"/success.html");
-}else{
-  res.sendFile(__dirname+"/failure.html");
-}
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
 
- response.on("data", function (data) {
-   console.log(JSON.parse(data));
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
 
- });
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
-request.write(jsonData);
-request.end();
-});
 
-app.post("/failure", function(req,res){
+app.post("/failure", function (req, res) {
   res.redirect("/"); //routs back to home root
 });
 
-app.listen(process.env.PORT || 3000,function () {
+app.listen(process.env.PORT || 3000, function () {
 
   console.log("Server running at port 3000");
 
